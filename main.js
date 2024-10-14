3c0ff33d886c8b74d0f03a9f0e2d2e6b0e6576a5
JovMan <174271476+JovTheMan@users.noreply.github.com>
2024-10-14 13:09:20 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Task 2: Selecting HTML Elements
+// Task 2 + 3: Selecting HTML Elements
 const productSelector = document.getElementById('product-selector');
 const quantityInput = document.getElementById('quantity');
 const totalPriceElement = document.getElementById('total-price');
